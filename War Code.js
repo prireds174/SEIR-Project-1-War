09def21667117540461a7eff8a605bc9389a58ee
prireds174 <97041885+prireds174@users.noreply.github.com>
2022-01-03 16:19:33 -0500

    Update War Code.js

--- War Code.js
+++ War Code.js
@@ -1,8 +1,15 @@
+console.log('BOOM!!')
 //Pseudocode
 
-/* Objects(variables)
+/* Objects
 -Cards
--New Game Button/Reset Button
+-New Game Button
+-Reset Button
+-Game Title Header
+-Player Turn Indicator
+-Winner
+-Card in Hand Counter
+
 
 
 Actions(functions)
@@ -10,14 +17,17 @@ Actions(functions)
     -Shuffling happens in both the beginning and also when readmitting cards to pile
 -The dealing of cards 
 -Have cards set up to respond to click action >>> flipping over to reveal 
+-Action of declaring war when similar cards are choosen
+
 
 
 Tracking:
 -Number of cards each player has in possession
+    -Loss of game is determined by complete lack of cards
 -Which Player deals the higher card value
     -Set hierarchy of cards
 
 
-*/
 
+*/
 
